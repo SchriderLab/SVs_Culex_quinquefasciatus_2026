69888936fad83f5355e8804ce3267f8e54c4e702
Remi Ketchum <remiketchum@gmail.com>
2026-09-07 14:31:53 +0100

    Update SVIM_asm.sh

--- SV_calling/SVIM_asm.sh
+++ SV_calling/SVIM_asm.sh
@@ -1,5 +1,5 @@
 #SVIM-asm calling commands run for each sample
-minimap2 -ax asm5 --cs -r2k -t 8 culex.qui.gcf.fasta /proj/dschridelab/remi/ANNA_RELAB/01_2026_FINAL2/01_GENOME_ASS/LZV1_GENOME_FINAL2.fasta > LZV1.sam
-samtools sort -m4G -@4 -o LZV1.sorted.bam LZV1.sam
-samtools index LZV1.sorted.bam
-svim-asm haploid ./LZV1 LZV1.sorted.bam culex.qui.gcf.fasta
+minimap2 -ax asm5 --cs -r2k -t 8 GCF_015732765.1_VPISU_Cqui_1.0_pri_paternal_genomic.fna [sample]_GENOME_FINAL2.fasta > [sample].sam
+samtools sort -m4G -@4 -o [sample].sorted.bam [sample].sam
+samtools index [sample].sorted.bam
+svim-asm haploid ./[sample] [sample].sorted.bam GCF_015732765.1_VPISU_Cqui_1.0_pri_paternal_genomic.fna
