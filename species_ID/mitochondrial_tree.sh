203d522a9eca23d765c9d052115f6ee7af9a59f0
Anna Trotter <143217631+annatrotter@users.noreply.github.com>
2026-09-07 20:56:42 -0400

    Update mitochondrial_tree.sh

--- species_ID/mitochondrial_tree.sh
+++ species_ID/mitochondrial_tree.sh
@@ -1,6 +1,6 @@
 #1 - run mitogenome assembly for each sample using MitoHiFi
-singularity exec --bind /work/users/a/n/annawt/SPECIESID_NEW/mitohifi/ --bind /proj/dschridelab/remi/ANNA_RELAB:/proj/dschridelab/remi/ANNA-RELAB docker://ghcr.io/marcelauliano/mitohifi:master mitohifi.py \
--r /proj/dschridelab/remi/ANNA_RELAB/01_2026_FINAL2_RNK/00_FASTQ/LZV1.fastq.gz -f NC_014574.1.fasta -g NC_014574.1.gb -t 72
+singularity exec --bind ./mitohifi docker://ghcr.io/marcelauliano/mitohifi:master mitohifi.py \
+-r LZV1.fastq.gz -f NC_014574.1.fasta -g NC_014574.1.gb -t 72
 
 #2 - rotate mitogenomes to a common starting point using Rotate
 ./rotate/rotate -s TCGCGACAATG -m 1 culex_aedes_mito.fa > culex_aedes_mito.rotated.fa
